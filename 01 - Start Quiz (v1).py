0319051b013686ce0fb7fa2f281ff7d09ade4b52
sychoe06 <83557960+sychoe06@users.noreply.github.com>
2023-05-05 11:30:50 +1200

    Create 01 - Start Quiz (v1).py

---
+++ 01 - Start Quiz (v1).py
@@ -0,0 +1,13 @@
+"""Component 1 - Start Quiz (version 1)
+Set up the Maori Quiz program
+"""
+
+import tkinter as tk
+root = tk.Tk()
+
+root.title("Maori Quiz")
+root.minsize(900, 600)  # sets minimum resizeable dimensions
+root.maxsize(1000, 700)
+
+
+root.mainloop()
