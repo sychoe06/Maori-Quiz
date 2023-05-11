73c2ac2c5049974851ca1c0f5b71908962066f8a
sychoe06 <83557960+sychoe06@users.noreply.github.com>
2023-05-12 10:59:44 +1200

    component 1

--- 01 - Start Quiz (v3).py
+++ 01 - Start Quiz (v3).py
@@ -1,10 +1,10 @@
-"""Component 1 - Start Quiz (version 2)
-Intro added
+"""Component 1 - Start Quiz (version 3)
+Button added
 """
 
 from tkinter import *
-root = Tk()
 
+root = Tk()
 root.title("Maori Quiz")
 root.minsize(900, 600)  # sets minimum resizeable dimensions
 root.maxsize(1000, 700)
@@ -16,5 +16,9 @@ Maori_Quiz_lbl = Label(root, text="Maori Quiz",
                        font=("Calibri", 50, "bold"), fg="red", bg="white")
 welcome_lbl.pack()
 Maori_Quiz_lbl.pack()
+
+# Start button
+start_btn = Button(root, text="START", fg="white", bg="black")
+start_btn.pack()
 
 root.mainloop()
