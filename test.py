35db9accf96b58d044ae0731ffac21eaefdcc177
sychoe06 <83557960+sychoe06@users.noreply.github.com>
2023-05-12 11:46:29 +1200

    component 1

--- test.py
+++ test.py
@@ -1,15 +1,48 @@
+# This will import all the widgets
+# and modules which are available in
+# tkinter and ttk module
 from tkinter import *
-import tkinter
+from tkinter.ttk import *
 
-top = tkinter.Tk()
-CheckVar1 = IntVar()
-CheckVar2 = IntVar()
-C1 = Checkbutton(top, text = "Music", variable = CheckVar1, \
-                 onvalue = 1, offvalue = 0, height=5, \
-                 width = 20)
-C2 = Checkbutton(top, text = "Video", variable = CheckVar2, \
-                 onvalue = 1, offvalue = 0, height=5, \
-                 width = 20)
-C1.pack()
-C2.pack()
-top.mainloop()
+# creates a Tk() object
+master = Tk()
+
+# sets the geometry of main
+# root window
+master.geometry("200x200")
+
+
+# function to open a new window
+# on a button click
+def openNewWindow():
+
+    # Toplevel object which will
+    # be treated as a new window
+    newWindow = Toplevel(master)
+
+    # sets the title of the
+    # Toplevel widget
+    newWindow.title("New Window")
+
+    # sets the geometry of toplevel
+    newWindow.geometry("200x200")
+
+    # A Label widget to show in toplevel
+    Label(newWindow,
+          text ="This is a new window").pack()
+
+
+label = Label(master,
+              text ="This is the main window")
+
+label.pack(pady = 10)
+
+# a button widget which will open a
+# new window on button click
+btn = Button(master,
+             text ="Click to open a new window",
+             command = openNewWindow)
+btn.pack(pady = 10)
+
+# mainloop, runs infinitely
+mainloop()
